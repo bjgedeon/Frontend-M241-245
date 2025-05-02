2b10bc295fd561ea9b9c659724a237858a9c6250
bjorngedeon <bjoern.gedeon@gmail.com>
2025-05-02 14:40:52 +0200

    dd server

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue'
+
+import vueDevTools from 'vite-plugin-vue-devtools'
+ 
 export default defineConfig({
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  server: {
+    host: true, 
+    
+    allowedHosts: [
+      'bbzw-horizon.duckdns.org'
+    ]
+  }
 })
